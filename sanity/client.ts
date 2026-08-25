37cc72a6360ea694ceea7626efadf42621d40832
KMF21 <ikoojojoseph817@gmail.com>
2026-08-25 16:04:31 +0100

    true-cdn

--- sanity/client.ts
+++ sanity/client.ts
@@ -4,5 +4,5 @@ export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   apiVersion: '2026-08-18', // use current date, keeps query behavior pinned
-  useCdn: false, // fast, cached reads — flip to false for instant-preview needs
+  useCdn: true, // fast, cached reads — flip to false for instant-preview needs
 })
